b701f6553c72a6b36ece4cb7807a709b113f1bfc
github_Sonu-Jalandara <sonukundanpura05@gmail.com>
2024-08-29 04:24:21 -0400

    api error discussion

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 //   require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
-  import connectDB from "./db/index.js";
+import connectDB from "./db/index.js";
 
-dotenv.config(
-    {
-        path:'./env'
-    }
-)
-
+dotenv.config({
+  path: "./env",
+});
 
 connectDB()
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server is running at Port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(" Mongo DB connection failed !!!", error);
+  });
 
 /*
 import express from "express";
